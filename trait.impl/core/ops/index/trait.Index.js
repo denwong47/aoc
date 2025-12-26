4414a4bbf0972a9fd8217c8245f20351a1049af1
denwong47 <denwong47@users.noreply.github.com>
2025-12-26 23:22:16 +0000

    deploy: 5c72ff2f7c0a6db0720bcf5cd865f6cb011be95e

--- trait.impl/core/ops/index/trait.Index.js
+++ trait.impl/core/ops/index/trait.Index.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["aoc_2025_12",[["impl&lt;const S: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"aoc_2025_12/models/struct.ShapeCounts.html\" title=\"struct aoc_2025_12::models::ShapeCounts\">ShapeCounts</a>&lt;S&gt;"]]]]);
+    var implementors = Object.fromEntries([["aoc_2025_12",[["impl&lt;const S: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.92.0/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.92.0/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"aoc_2025_12/models/struct.ShapeCounts.html\" title=\"struct aoc_2025_12::models::ShapeCounts\">ShapeCounts</a>&lt;S&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/simple_graph/traits/trait.IsNode.js
+++ trait.impl/simple_graph/traits/trait.IsNode.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["aoc_2025_11",[["impl&lt;'s&gt; <a class=\"trait\" href=\"simple_graph/traits/trait.IsNode.html\" title=\"trait simple_graph::traits::IsNode\">IsNode</a>&lt;'s, <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.u32.html\">u32</a>, <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.91.1/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"aoc_2025_11/models/struct.Device.html\" title=\"struct aoc_2025_11::models::Device\">Device</a>"]]]]);
+    var implementors = Object.fromEntries([["aoc_2025_11",[["impl&lt;'s&gt; <a class=\"trait\" href=\"simple_graph/traits/trait.IsNode.html\" title=\"trait simple_graph::traits::IsNode\">IsNode</a>&lt;'s, <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.92.0/std/primitive.u32.html\">u32</a>, <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.92.0/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"aoc_2025_11/models/struct.Device.html\" title=\"struct aoc_2025_11::models::Device\">Device</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
